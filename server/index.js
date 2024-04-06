f14bc5ee2cc3f3edba758a0e868fb5a8cb824135
Awais Malik <165843085+19497awaismalik@users.noreply.github.com>
2024-04-06 14:37:14 +0500

    Update index.js

--- server/index.js
+++ server/index.js
@@ -10,7 +10,7 @@ const port = 5000;
 app.use(express.json());
 
 app.use(cors({
-  origin:"*",
+  origin:"https://inotebook-mern-frontend.vercel.app",
   methods:["POST","GET","DELETE","PUT"],
   credentials:true
 }))
