ea76cca3ffe64ddb02efec8690cfb3b14b323088
Awais Malik <165843085+19497awaismalik@users.noreply.github.com>
2024-04-06 14:35:28 +0500

    Update NoteState.js

--- frontend/src/components/context/notes/NoteState.js
+++ frontend/src/components/context/notes/NoteState.js
@@ -2,7 +2,7 @@ import React,{useState} from "react";
 
 import NoteContext from "./noteContext";
 const NoteState = (props) => {
- const host="http://localhost:5000"
+ const host="https://inotebook-mern-server.vercel.app"
 
   
 let post=[]
